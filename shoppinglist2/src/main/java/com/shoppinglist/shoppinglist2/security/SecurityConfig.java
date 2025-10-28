a344c26dbe218137427bd84711b6a73bcbe0ad40
raedered <kjh991216@naver.com>
2025-10-28 17:44:47 +0900

    feat: 20251028 Google OAuth2 Backend

--- shoppinglist2/src/main/java/com/shoppinglist/shoppinglist2/security/SecurityConfig.java
+++ shoppinglist2/src/main/java/com/shoppinglist/shoppinglist2/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     private final UserDetailsServiceImpl userDetailsService;
     private final JwtService jwtService;
     private final AuthEntryPoint authEntryPoint;
+    private final CustomOauth2SuccessHandler customOauth2SuccessHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -50,23 +51,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화(Stateless JWT 사용)
-//                .cors(Customizer.withDefaults())    // CORS 설정(이하의 설정 사용)
-//                .sessionManagement(session ->
-//                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
-//                .authorizeHttpRequests(auth -> auth
-//                        // /login 엔드포인트의 POST 요청은 모두 허용
-//                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
-//                        .requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
-//                        .anyRequest().authenticated())
-//                .exceptionHandling(ex -> ex
-//                        .authenticationEntryPoint(authEntryPoint)) // 인증 실패 시 처리
-//                .addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
+        http.csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화(Stateless JWT 사용)
+                .cors(Customizer.withDefaults())    // CORS 설정(이하의 설정 사용)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        // /login 엔드포인트의 POST 요청은 모두 허용
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
+                        .requestMatchers("/oauth2/**", "/login/oauth2/code/*").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2.successHandler(customOauth2SuccessHandler))
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authEntryPoint)) // 인증 실패 시 처리
+                .addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
         // 개발 중 로그인 포함 모든 HTTP 메서드 요청 허용
-        http.csrf(csrf -> csrf.disable())
-                .cors(withDefaults())
-                .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests.anyRequest().permitAll());
+//        http.csrf(csrf -> csrf.disable())
+//                .cors(withDefaults())
+//                .authorizeHttpRequests(authorizeHttpRequests ->
+//                        authorizeHttpRequests.anyRequest().permitAll());
         return http.build();
     }
 
